2669353c386da417ad9f5888a067f522b8c48237
heTTTttt <rock.n.roll.one.love@gmail.com>
2022-12-30 23:00:12 +0200

    Initial commit

--- src/main/java/PetrolStation.java
+++ src/main/java/PetrolStation.java
@@ -5,7 +5,7 @@ public class PetrolStation {
     void doRefuel(double fuelAmount){
         double remainingFuel = amount - fuelAmount;
         if (amount < fuelAmount){
-            System.out.println("Does not have fuel amount what you need ");
+            System.out.println("Does not have the fuel amount that you need ");
         }
     }
 }
