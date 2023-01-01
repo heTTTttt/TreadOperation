85b355e666492f6935f32684eeea58773f1a29bd
heTTTttt <rock.n.roll.one.love@gmail.com>
2023-01-01 21:24:12 +0200

    Initial commit

--- src/main/java/PetrolStation.java
+++ src/main/java/PetrolStation.java
@@ -1,11 +1,37 @@
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PetrolStation {
-    double amount;
+    public volatile double amount = 1000;
+    private final Object objectLock = new Object();
+    private AtomicInteger counter = new AtomicInteger();
 
-    void doRefuel(double fuelAmount){
+    synchronized void doRefuel() {
+
+        //synchronized (objectLock){
+            if (counter.getAndIncrement() > 2){
+                System.out.println("To much objects");
+                return;
+            }
+        //}
+        System.out.println("Before amount " + amount+ " count of obj " + counter);
+
+        double fuelAmount = 20 + Math.random() * 40;
+        System.out.println(fuelAmount);
         double remainingFuel = amount - fuelAmount;
-        if (amount < fuelAmount){
-            System.out.println("Does not have the fuel amount that you need ");
-        }
+        //synchronized (objectLock) {
+
+            if (remainingFuel < fuelAmount) {
+                System.out.println("Does not have the fuel amount that you need ");
+                return;
+            }
+            try {
+                Thread.sleep((long) (Math.random() * 7) + 3);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            amount -= fuelAmount;
+        //}
+        System.out.println("After amount " + amount);
+        counter.getAndDecrement();
     }
 }
